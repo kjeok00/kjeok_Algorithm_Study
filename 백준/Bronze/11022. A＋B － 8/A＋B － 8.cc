ed4b6a1ac2f5031886504a5897a7377a17d80c4f
김지혁 <kjeok00@gmail.com>
2024-07-05 18:22:44 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T;
+	int a, b;
+
+	cin >> T;
+
+	for (int i = 0; i < T; i++)
+	{
+		cin >> a >> b;
+		cout << "Case #" << i + 1 << ": " << a << " + " << b << " = " << a + b << '\n';
+	}
+
+
+	return 0;
+}
