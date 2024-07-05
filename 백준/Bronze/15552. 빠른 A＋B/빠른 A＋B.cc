bcc795268efe2e8e7673f8be2339f435241588c9
김지혁 <kjeok00@gmail.com>
2024-07-05 18:23:12 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 216 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	iostream::sync_with_stdio(false);
+	
+	int T;
+	int a, b;
+
+	cin >> T;
+
+	for (int i = 0; i < T; i++)
+	{
+		cin >> a >> b;
+		cout << a + b <<'\n';
+	}
+
+
+
+
+	return 0;
+}
