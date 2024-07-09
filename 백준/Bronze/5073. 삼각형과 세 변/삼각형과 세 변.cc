949edbc4b8e99be0b4e6abfff2b3e3e86eff9684
김지혁 <kjeok00@gmail.com>
2024-07-09 18:13:27 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.cc
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.cc
@@ -1,34 +1,27 @@
 #include <iostream>
-#include <algorithm>
+#include <algorithm> 
 using namespace std;
 
-int main() {
+int main()
+{
     while (true) {
         int a1, a2, a3;
         cin >> a1 >> a2 >> a3;
-
-        // 입력이 "0 0 0"인 경우 루프 종료
         if (a1 == 0 && a2 == 0 && a3 == 0) {
             break;
         }
 
-        // 세 변의 합 계산
         int sum = a1 + a2 + a3;
-
-        // 가장 긴 변 찾기
-        int max_side = max({a1, a2, a3});
-
-        // 삼각형이 유효한지 확인
-        if (max_side >= sum - max_side) {
+        int max_angle = max({ a1, a2, a3 });
+    
+        if (max_angle >= sum - max_angle)
             cout << "Invalid" << endl;
-        } else if (a1 == a2 && a2 == a3) {
+        else if (a1 == a2 && a2 == a3 && a1 == a3)
             cout << "Equilateral" << endl;
-        } else if (a1 == a2 || a2 == a3 || a1 == a3) {
-            cout << "Isosceles" << endl;
-        } else {
+        else if (a1 != a2 && a2 != a3 && a1 != a3)
             cout << "Scalene" << endl;
-        }
+        else
+            cout << "Isosceles" << endl;
     }
-
     return 0;
 }
