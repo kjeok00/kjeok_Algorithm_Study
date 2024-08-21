202a91c53a9383912e4abc071db9048485c3def7
김지혁 <kjeok00@gmail.com>
2024-08-21 19:55:17 +0900

    [Silver V] Title: 영화감독 숌, Time: 500 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,11 +1,10 @@
 n = int(input())
-
-count = 0
 num = 666
+count = 0
 
 while True:
     if '666' in str(num):
-        count += 1
+        count+=1
         if count == n:
             print(num)
             break
