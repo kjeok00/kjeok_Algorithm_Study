d8124773905c49b655f76b36d4eb1f2a4206ab37
김지혁 <kjeok00@gmail.com>
2024-09-22 16:30:04 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,13 +1,14 @@
-class Solution {
+
+    class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
         StringBuilder sb = new StringBuilder();
-        // my_string의 s 이전 부분을 추가
+        // s 이전 부분 추가
         sb.append(my_string.substring(0, s));
-        // overwrite_string 전체를 추가
+        // overwrite_string 추가
         sb.append(overwrite_string);
-        // my_string의 s + overwrite_string.length() 이후 부분을 추가
+        // s + overwrite_string.length() 이후 부분 추가
         sb.append(my_string.substring(s + overwrite_string.length()));
         
-        return sb.toString();  // 최종 문자열을 반환
+        return sb.toString();  // 최종 문자열 반환
     }
 }
