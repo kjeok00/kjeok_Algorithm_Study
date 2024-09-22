9896cd3e71e2911b548cd0093623868abbc7b41b
김지혁 <kjeok00@gmail.com>
2024-09-22 15:25:52 +0900

    [level 0] Title: 특수문자 출력하기, Time: 122.82 ms, Memory: 59.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append('!');
+        sb.append('@');
+        sb.append('#');
+        sb.append('$');
+        sb.append('%');
+        sb.append('^');
+        sb.append('&');
+        sb.append('*');
+        sb.append('(');
+        sb.append('\\');
+        sb.append("'");
+        sb.append('"');
+        sb.append('<');
+        sb.append('>');
+        sb.append('?');
+        sb.append(':');
+        sb.append(';');
+        
+        System.out.println(sb.toString());
+    }
+}
