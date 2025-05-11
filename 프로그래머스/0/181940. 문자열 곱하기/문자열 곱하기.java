038f81592c5b443f09410268d30aff190ee366cb
김지혁 <kjeok00@gmail.com>
2025-05-11 17:29:43 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.07 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,10 +1,13 @@
 class Solution {
     public String solution(String my_string, int k) {
         String answer = "";
+        StringBuilder sb = new StringBuilder();
         
-        for(int i = 0; i < k; i++) {
-            answer += my_string;
+        for (int i = 0; i < k; i++) {
+            sb.append(my_string);
         }
+        
+        answer = sb.toString();
         
         return answer;
     }
