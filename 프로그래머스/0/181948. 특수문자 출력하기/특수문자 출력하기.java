7d9a06693792c81fdbcf7a9a1d9df8533a845f53
김지혁 <kjeok00@gmail.com>
2025-05-11 16:28:56 +0900

    [level 0] Title: 특수문자 출력하기, Time: 157.73 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -1,27 +1,5 @@
-import java.util.Scanner;
-
 public class Solution {
-    public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        
-        sb.append('!');
-        sb.append('@');
-        sb.append('#');
-        sb.append('$');
-        sb.append('%');
-        sb.append('^');
-        sb.append('&');
-        sb.append('*');
-        sb.append('(');
-        sb.append('\\');
-        sb.append("'");
-        sb.append('"');
-        sb.append('<');
-        sb.append('>');
-        sb.append('?');
-        sb.append(':');
-        sb.append(';');
-        
-        System.out.println(sb.toString());
+    public static void main(String[] args){
+        System.out.println("!@#$%^&*(\\\'\"<>?:;");
     }
 }
