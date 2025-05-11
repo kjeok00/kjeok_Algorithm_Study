d5759d14419b6c92f4bf90ed22cce07aa3d6204d
김지혁 <kjeok00@gmail.com>
2025-05-11 16:37:32 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 190.27 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.java
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int a = sc.nextInt();
         int b = sc.nextInt();
-
-        System.out.println(a + " + " + b + " = " + (a+b));
+        
+        int sum = a + b;
+        
+        System.out.print(a + " + " + b + " = " + sum);
     }
 }
